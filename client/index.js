d6f49d02fe108483bacc06f7d00696eb02b533a0
piggypiggyyoinkyoink <quackquack904@gmail.com>
2026-06-17 22:31:47 +0100

    basic functionality achieved

--- client/index.js
+++ client/index.js
@@ -3,41 +3,58 @@ const height = 1000;
 
 const svg = d3.select("#map");
 let projection;
-fetch("/static/gb.json")
-    .then(response => response.json())
-    .then(geoData => {
-        // Create projection
-        projection = d3.geoConicConformal()
-            .fitSize([width, height], geoData);
+window.addEventListener("DOMContentLoaded", async () => {
+    let response = await fetch("/static/gb.json");
+    let geoData = await response.json();
+    // Create projection
+    projection = d3.geoConicConformal()
+        .fitSize([width, height], geoData);
 
-        // Create path generator
-        const path = d3.geoPath()
-            .projection(projection);
+    // Create path generator
+    const path = d3.geoPath()
+        .projection(projection);
 
-        // Draw all features
-        svg.selectAll("path")
-            .data(geoData.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("fill", "#eeeeee")
-            .attr("stroke", "#444")
-            .attr("stroke-width", 0.5);
+    // Draw all features
+    svg.selectAll("path")
+        .data(geoData.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "#eeeeee")
+        .attr("stroke", "#444")
+        .attr("stroke-width", 0.5);
 
+    function addPlace(place) {
+
+        const [x, y] = projection([
+            place.longitude,
+            place.latitude
+        ]);
+
+        svg.append("circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", 4)
+            .attr("fill", "rgba(255, 0, 0, 0.5)");
+    }
+    document.getElementById("placeInput").addEventListener("input", async (event) => {
+        // console.log("hello");
+        const placeName = event.target.value;
+        if (placeName.length >= 2) {
+            const response = await fetch(`http://127.0.0.1:8000/query?text=${encodeURIComponent(placeName)}`);
+            const places = await response.json();
+            // console.log(places.results);
+            if (places.results.length > 0) {
+                // console.log("beep");
+                for (const place of places.results) {
+                    addPlace({ latitude: place.lat, longitude: place.lon });
+                }
+                event.target.value = "";
+            }
+        }
     });
+    // addPlace({ latitude: 51.5074, longitude: -0.1278 }); // Example: London
+});
 
-function addPlace(place) {
 
-    const [x, y] = projection([
-        place.longitude,
-        place.latitude
-    ]);
 
-    svg.append("circle")
-        .attr("cx", x)
-        .attr("cy", y)
-        .attr("r", 40)
-        .attr("fill", "red");
-}
-
-addPlace({ latitude: 51.5074, longitude: -0.1278 }); // Example: London
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:*",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="client"), name="static")
 
@@ -15,6 +30,8 @@ async def query(text: str):
 
     results = cur.fetchall()
     con.close()
-    if not results:
-        return None
-    return {"results": results}
+    response = {"results": []}
+    for result in results:
+        res_json = {"name": result[0], "lat": result[1], "lon": result[2]}
+        response["results"].append(res_json)
+    return response
